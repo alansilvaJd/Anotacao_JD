bcd9bb3a0fc42c76f33bcdfa8feb318f1baec713
alansilvaJd <alan.silva@jdconsultores.com.br>
2026-01-06 17:19:35 -0300

    Add files via upload

---
+++ Caminhos/NomeArquivos.sql
@@ -0,0 +1,11 @@
+101 - incluir título
+101R - retorno do título incluindo
+
+102 - Alteração Título
+102R -Retorno da alteração do título
+
+108 - Baixa do título
+108R- Retorno da baixa do título.
+
+116  - Cancelamento
+116R - Retorno Cancelamento 
